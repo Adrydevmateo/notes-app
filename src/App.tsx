fb8d335eab0a3a0e18856d540ddc134c80f57206
AdryDev <adrydevmateo@gmail.com>
2024-04-18 00:17:57 -0400

    removed setter from notes state

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TNote } from './App.types'
 
 // TODO: Add Sorting Controls
 function App() {
-  const [notes, setNotes] = useState<Array<TNote>>([
+  const [notes] = useState<Array<TNote>>([
     { id: crypto.randomUUID(), title: 'Title', content: 'Content' },
     { id: crypto.randomUUID(), title: 'Title', content: 'Content' },
     { id: crypto.randomUUID(), title: 'Title', content: 'Content' },
