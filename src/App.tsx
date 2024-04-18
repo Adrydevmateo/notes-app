7a7b9e7ad6a72ee845df001b81c3280cfe5eef03
AdryDev <adrydevmateo@gmail.com>
2024-04-17 21:34:54 -0400

    Loading Screen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import './App.css'
 
+// TODO: Add Sorting Controls
 function App() {
-  console.log('Hello World');
-
-
   return (
-    <>
-      <h1>Notes</h1>
-    </>
+    <main>
+      <header>
+        <h1>Notes</h1>
+      </header>
+
+      <div className='note'>
+        <h2 className='title'>Title</h2>
+        <div className='content'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis voluptatibus culpa porro at fuga reprehenderit optio exercitationem doloribus veritatis minima inventore atque voluptate maiores dolore amet nostrum, repudiandae reiciendis pariatur?
+        </div>
+      </div>
+
+      <button type='button' id='btn-add-more'>+</button>
+    </main>
   )
 }
 
